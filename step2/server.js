13c65e9e3eaab16e6bdb9ec1be0f3c0f67b62217
Lothindir <lothindir@gmail.com>
2021-12-21 20:41:41 +0100

    Add step 2

---
+++ step2/server.js
@@ -0,0 +1,48 @@
+import Fastify from 'fastify';
+import Chance from 'chance';
+
+const fastify = Fastify({
+   logger: true
+});
+const chance = Chance();
+const port = 3333;
+
+fastify.get('/', (request, reply) => {
+   reply.send(generate());
+});
+
+const start = async () => {
+   try {
+     await fastify.listen(port, '0.0.0.0')
+   } catch (err) {
+     fastify.log.error(err)
+     process.exit(1)
+   }
+ }
+
+function generate() {
+   let pilotsNumber = chance.natural({ min: 5, max: 50 });
+   let pilots = [];
+   for (let i = 0; i < pilotsNumber; i++){
+      let family = [];
+      let familyMembers = chance.natural({ min: 1, max: 5 });
+      for (let j = 0; j < familyMembers; j++){
+         let familyMember = {
+            name: chance.first(),
+            age: chance.age(),
+         };
+         family.push(familyMember);
+      }
+      let pilot = {
+         name: chance.name(),
+         age: chance.age({ type: 'adult' }),
+         callSign: chance.radio(),
+         favoriteAnimal: chance.animal(),
+         family: family,
+      }
+      pilots.push(pilot);
+   }
+   return pilots;
+}
+
+start()
